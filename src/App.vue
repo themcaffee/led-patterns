0c8fe2f4420c634eea0db34973cf194eef84da68
Mitch McAffee <themcaffee@gmail.com>
2018-06-23 05:51:30 -0600

    Working on basic grid

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,69 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <HelloWorld/>
+  <div class="container" id="app">
+    <h2 class="text-center">LED Pattern Generator</h2>
+    <br>
+    <div class="row">
+      <form class="form-inline">
+        <div class="col-5 form-group">
+          <label class="col-5" for="height-input">Height</label>
+          <input type="text" class="form-control col-5" id="height-input" v-model.number="height" />
+        </div>
+        <div class="col-5 form-group">
+          <label class="col-5" for="frames-input">Frames</label>
+          <input type="text" class="form-control col-5" id="frames-input" v-model.number="frames" />
+        </div>
+        <button class="col-2 btn btn-default" @click="changeSize()">Change Size</button>
+      </form>
+    </div>
+    <div v-for="frame in leds" :key="frame" class="row">
+      <div v-for="row in frame" :key="row">
+        <span class="led-box">&nbsp;</span>
+      </div>
+    </div>
   </div>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld'
+<style>
+.led-box {
+  height: 25px;
+  width: 25px;
+  background-color: #D3D3D3;
+  margin-left: 2px;
+  margin-right: 2px;
+  margin-top: 2px;
+  margin-bottom: 2px;
+  padding-left: 5px;
+  padding-right: 5px;
+  padding-bottom: 5px;
+}
+</style>
 
+<script>
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  data () {
+    return {
+      height: 49,
+      frames: 1,
+      leds: [[]]
+    }
+  },
+
+  mounted () {
+    this.changeSize()
+  },
+
+  methods: {
+    changeSize () {
+      this.leds = []
+      for (var colIndex = 0; colIndex < this.frames; colIndex++) {
+        let col = []
+        for (var rowIndex = 0; rowIndex < this.height; rowIndex++) {
+          col.push('D3D3D3')
+        }
+        this.leds.push(col)
+      }
+    }
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
